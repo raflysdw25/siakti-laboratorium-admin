ffdf95a29dae332859141e138db6555b810f56c0
firmansyahgadjeman <gajeness@gmail.com>
2020-05-13 13:34:53 +0700

    template yang udah lebih bener

--- application/views/template.php
+++ application/views/template.php
@@ -5,28 +5,28 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Website Inventaris Barang Jurusan TIK</title>
   <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/fontawesome-free/css/all.min.css"/>
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"/>
   <!-- iCheck -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css"/>
   <!-- JQVMap -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/jqvmap/jqvmap.min.css"/>
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/dist/css/adminlte.min.css"/>
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"/>
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/daterangepicker/daterangepicker.css"/>
   <!-- summernote -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/summernote/summernote-bs4.css"/>
   <!-- DataTables -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css"/>
+  <link rel="stylesheet" href="<?php base_url() ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css"/>
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -66,7 +66,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.html" class="brand-link">
-      <img src="<?= base_url() ?>assets/dist/img/PNJLogo.png" alt="Politeknik Negeri Jakarta Logo" class="brand-image">
+      <img src="<?php base_url() ?>assets/dist/img/PNJLogo.png" alt="Politeknik Negeri Jakarta Logo" class="brand-image"/>
       <span class="brand-text font-weight-light">Sistem Informasi TIK</span>
     </a>
 
@@ -75,7 +75,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?php base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"/>
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -130,9 +130,9 @@
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!--   <div class="content-wrapper">
     <?= $contents ?>
-  </div>
+  </div> -->
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <strong>Copyright &copy; 2020 Laboratorium Jurusan TIK.</strong>
@@ -148,39 +148,39 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="<?= base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="<?= base_url() ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="<?= base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="<?= base_url() ?>assets/plugins/chart.js/Chart.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="<?= base_url() ?>assets/plugins/sparklines/sparkline.js"></script>
+<script src="<?php base_url() ?>assets/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
 <!-- <script src="<?= base_url() ?>assets/plugins/jqvmap/jquery.vmap.min.js"></script> -->
 <!-- <script src="<?= base_url() ?>assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script> -->
 <!-- jQuery Knob Chart -->
-<script src="<?= base_url() ?>assets/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="<?= base_url() ?>assets/plugins/moment/moment.min.js"></script>
-<script src="<?= base_url() ?>assets/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="<?php base_url() ?>assets/plugins/moment/moment.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="<?= base_url() ?>assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="<?= base_url() ?>assets/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="<?= base_url() ?>assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="<?php base_url() ?>assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?= base_url() ?>assets/dist/js/adminlte.js"></script>
+<script src="<?php base_url() ?>assets/dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?= base_url() ?>assets/dist/js/pages/dashboard.js"></script>
+<script src="<?php base_url() ?>assets/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?= base_url() ?>assets/dist/js/demo.js"></script>
+<script src="<?php base_url() ?>assets/dist/js/demo.js"></script>
 
 <!-- DataTables -->
 <script src="<?= base_url() ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
